0439d319a7803d1c53383554ec0aff56d79e6158
ggo <ggo@clickndecide.com>
2022-11-08 17:41:51 +0100

    1st commit

---
+++ src/testEF6MySQL2/testEF6MySQL2/Program.cs
@@ -0,0 +1,36 @@
+﻿using DsnPlus.Core.Exceptions;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testEF6MySQL2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection("server=localhost;port=3306;database=testCodeFirst;uid=root;password=admin"))
+                {
+                    conn.Open();
+
+                    using (var ctx = new testCodeFirstContext(conn, false))
+                    {
+                        var query = (from data in ctx.sales
+                                     select data).ToList();
+                        var firstSalesman = query.First().salesman;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetMessage());
+            }
+        }
+    }
+}
---
+++ src/testEF6MySQL2/testEF6MySQL2/testCodeFirstContext.cs
@@ -0,0 +1,64 @@
+﻿using MySql.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testEF6MySQL2
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class testCodeFirstContext : DbContext
+    {
+        public DbSet<salesman> salesman { get; set; }
+        public DbSet<sales> sales { get; set; }
+
+        public testCodeFirstContext()
+          : base()
+        {
+
+        }
+
+        public testCodeFirstContext(DbConnection existingConnection, bool contextOwnsConnection)
+          : base(existingConnection, contextOwnsConnection)
+        {
+
+        }
+    }
+
+    [Table("salesman")]
+    public class salesman
+    {
+        [Key]
+        public int sal { get; set; }
+
+        public string salname { get; set; }
+        public string mail { get; set; }
+        public string area { get; set; }
+    }
+
+    [Table("sales")]
+    public class sales
+    {
+        [Key]
+        [StringLength(5)]
+        public string no { get; set; }
+
+        public int cust { get; set; }
+
+        [ForeignKey("salesman")]
+        public int sal { get; set; }
+
+        public DateTime? date { get; set; }
+
+        public decimal datenum { get; set; }
+
+        public double total { get; set; }
+
+        public virtual salesman salesman { get; set; }
+    }
+}
